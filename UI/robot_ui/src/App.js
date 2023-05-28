093b9a370a5f51103796deaf042138b4122e8858
ret7020 <ret7020@gmail.com>
2023-05-28 08:05:34 +0300

    Robot UI ant design + menu

--- UI/robot_ui/src/App.js
+++ UI/robot_ui/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { BellOutlined, CodeOutlined, ControlOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme } from 'antd';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const { Content, Sider } = Layout;
 
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  const menu = [
+    {
+      key: '0',
+      icon: <BellOutlined />,
+      label: "Intro"
+    },
+    {
+      key: '1',
+      icon: <ControlOutlined />,
+      label: "Joystick"
+    },
+    {
+      key: '2',
+      icon: <CodeOutlined />,
+      label: "SPI Terminal"
+    }
+  ];
+  return (
+    <Layout>
+      <Sider
+        className="main-slider"
+        breakpoint="lg"
+        collapsedWidth="0"
+        style={{
+          height: '100vh',
+        }}
+
+      >
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['0']}
+          items={menu}
+        />
+      </Sider>
+      <Layout>
+
+        <Content
+          style={{
+            margin: '24px 16px 0',
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Content
+          </div>
+        </Content>
+
+      </Layout>
+    </Layout>
+  );
+};
 export default App;
