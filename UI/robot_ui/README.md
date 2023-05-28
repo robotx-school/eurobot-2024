97b24441c6ae86d189d4cba92cadcaf976676cfe
ret7020 <ret7020@gmail.com>
2023-05-28 09:08:34 +0300

    Add liquid RPi charts

--- UI/robot_ui/README.md
+++ UI/robot_ui/README.md
@@ -1,2 +1,5 @@
 # Robot Debug UI
-Use for mechanics debug and development
+![image](https://github.com/robotx-school/eurobot-2024/assets/55328925/1a8d3878-8822-45c1-bbf5-967edbde7bf4)
+
+Use for mechanics debug and development.
+
--- UI/robot_ui/src/App.js
+++ UI/robot_ui/src/App.js
@@ -1,9 +1,30 @@
-import { BellOutlined, CodeOutlined, ControlOutlined } from '@ant-design/icons';
-import { Layout, Menu, theme } from 'antd';
+import { BellOutlined, CodeOutlined, ControlOutlined, FundOutlined } from '@ant-design/icons';
+import { Liquid } from '@ant-design/plots';
+import { Layout, Menu, Space, theme, Typography } from 'antd';
 import React from 'react';
 import './App.css';
 
 const { Content, Sider } = Layout;
+const { Text } = Typography;
+
+const UsageLiquid = ({ percent, title, color }) => (
+  <Space direction='vertical' align='center'>
+    <Liquid {...{
+      color: color,
+      width: 200,
+      height: 200,
+      percent: percent,
+      outline: {
+        border: 4,
+        distance: 4,
+      },
+      wave: {
+        length: 128,
+      }
+    }} />
+    <Text strong>{title}</Text>
+  </Space>
+)
 
 const App = () => {
   const {
@@ -12,18 +33,33 @@ const App = () => {
   const menu = [
     {
       key: '0',
-      icon: <BellOutlined />,
-      label: "Intro"
+      icon: <FundOutlined />,
+      label: "Stats"
     },
     {
       key: '1',
+      icon: <BellOutlined />,
+      label: "Motors"
+    },
+    {
+      key: '2',
+      icon: <BellOutlined />,
+      label: "Servos"
+    },
+    {
+      key: '3',
       icon: <ControlOutlined />,
       label: "Joystick"
     },
     {
-      key: '2',
+      key: '4',
       icon: <CodeOutlined />,
       label: "SPI Terminal"
+    },
+    {
+      key: '5',
+      icon: <CodeOutlined />,
+      label: "CTD Terminal"
     }
   ];
   return (
@@ -59,7 +95,13 @@ const App = () => {
               background: colorBgContainer,
             }}
           >
-            Content
+            <h3>RPi Statistic</h3>
+            <Space direction='horizontal' size='large' style={{ width: '100%', justifyContent: 'center' }}>
+              <UsageLiquid percent={0.9} color={"#8BC34A"} title={"CPU"} />
+              <UsageLiquid percent={0.6} color={"#4ac387"} title={"RAM"} />
+              <UsageLiquid percent={0.3} color={"#c34a4a"} title={"SD"} />
+            </Space>
+
           </div>
         </Content>
 
